62d7441c4767d00c0fd27bdcc4d8d89b0af8e924
WhyIsEvery4thYearAlwaysBad <amicdict163@gmail.com>
2020-11-18 17:05:27 -0600

    Fix: Duplicate pages not appending properly.

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -311,26 +311,23 @@ void MenuCreate(unsigned short& bindcount) {
 				//For binds to pages.
 				tpage=static_cast<Parser::PageToken&>(**t);
 				std::size_t i=0llu;
-				pagestack.push_front({Page(tpage.Name),tpage.depth});	
-				if (pagestack.size()>=2) (pagestack.begin()+1)->first.binds.push_back(Bind(nkeystack.top(),tpage.Name,"exec _cvm_page_"+Format(tpage.Name)));
+				// Form duplicates if formatted name is already taken.
 				for (auto& t : pagestack)
 				{
-					if (t.first.formatted_title==pagestack.front().first.formatted_title) {
-						i++;
-					}
+					if (t.first.title==tpage.Name) i++;
 				}
 				for (auto& t : pages)
 				{
-					if (t.first.formatted_title==pagestack.front().first.formatted_title) {
-						i++;
-					}
+					if (t.first.title==tpage.Name) i++;
 				}
-				if (i>=2) {
-					pagestack.front().first.formatted_title+='_'+std::to_string(i-1);
-					if (pagestack.size()>=2) (pagestack.begin()+1)->first.binds.back().cmdstr.front()+='_'+std::to_string(i-1);
+				pagestack.push_front({Page(tpage.Name),tpage.depth});	
+				if (i>0) pagestack.front().first.formatted_title+='_'+std::to_string(i);
+				if (pagestack.size()>1) {
+					if (i>0) (pagestack.begin()+1)->first.binds.push_back(Bind(nkeystack.top(),tpage.Name,"exec $pageopen_"+Format(tpage.Name)+'_'+std::to_string(i)));
+					else (pagestack.begin()+1)->first.binds.push_back(Bind(nkeystack.top(),tpage.Name,"exec $pageopen_"+Format(tpage.Name)));
 				}
+				nkeystack.push(1u);
 			}
-			nkeystack.push(1u);
 			break;
 		case Parser::MenuTokenType::MENU_END_PAGE:
 			pages.push_front(pagestack.front());
