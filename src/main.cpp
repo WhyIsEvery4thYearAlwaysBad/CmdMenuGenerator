734ef453be367c3f9ed86b367343811652d3bf39
WhyIsEvery4thYearAlwaysBad <amicdict163@gmail.com>
2020-11-18 21:06:10 -0600

    Extra stuff was missing.

--- src/main.cpp
+++ src/main.cpp
@@ -30,19 +30,19 @@ int main(int argc, char** argv) {
 		ShowHelp();
 		return -1;
 	}
-	
-	// bool overrideexists=false;
 	std::string Line, InFileContent; 
 	std::ifstream inputf(inputfilename,std::ios_base::binary);
 	while (std::getline(inputf,Line)) {
 		InFileContent+=Line+'\n';
 	}
+	std::cout<<"Compiling.\n";
 	if (!Tokenize(InFileContent) || !Parser::ParseTokens()) {
 		for (auto& e : errors) {
 			std::cout<<e.val<<'\n';
 		}
 		return -1;
 	}
+	std::cout<<"Done. Creating Menus.\n";
 	// Creating parts from Menu tokens.
 	MenuCreate(bindcount);
 	// Directories
@@ -52,27 +52,14 @@ int main(int argc, char** argv) {
 	// Main exec.
 	std::ofstream exec(outputdir.string()+"/cfg/activate_cvm.cfg");
 	exec<<R"(closecaption 1
-cc_lang _customvoicemenu
+cc_lang customvoicemenu
 alias _cvm.nullkeys "alias _cvm.1 ; alias _cvm.2 ; alias _cvm.3 ; alias _cvm.4 ; alias _cvm.5 ; alias _cvm.6 ; alias _cvm.7 ; alias _cvm.8 ; alias _cvm.9"
-alias cvm.exitmenu "cc_emit _#cvm.clear_screen; alias +cvm.openmenu _cvm.cvmstate0; _cvm.resetkeys; cc_linger_time )"<<keymap["#cvm.linger_time"]<<"; cc_predisplay_time "<<keymap["#cvm.predisplay_time"]<<'\"';
+alias cvm.exitmenu "cc_emit _#cvm.clear_screen; )"<<keymap["#cvm.resetkeys"]<<"; cc_linger_time "<<keymap["#cvm.linger_time"]<<"; cc_predisplay_time "<<keymap["#cvm.predisplay_time"]<<'\"';
 	exec<<R"(
-alias +cvm.openmenu _cvm.cvmstate0
-alias -cvm.openmenu ;
-alias _cvm.cvmstate0 "cc_linger_time 10000; cc_predisplay_time 0; _cvm.openmenu; alias -cvm.openmenu alias +cvm.openmenu _cvm.cvmstate1; bind 1 _cvm.1; bind 2 _cvm.2; bind 3 _cvm.3; bind 4 _cvm.4; bind 5 _cvm.5; bind 6 _cvm.6; bind 7 _cvm.7; bind 8 _cvm.8; bind 9 _cvm.9"
-alias _cvm.cvmstate1 "cvm.exitmenu; alias -cvm.openmenu alias +cvm.openmenu _cvm.cvmstate0"
+alias _cvm.menusettings "cc_linger_time 10000; cc_predisplay_time 0; bind 1 _cvm.1; bind 2 _cvm.2; bind 3 _cvm.3; bind 4 _cvm.4; bind 5 _cvm.5; bind 6 _cvm.6; bind 7 _cvm.7; bind 8 _cvm.8; bind 9 _cvm.9"
 cvm.exitmenu
-alias _cvm.resetkeys ")"<<keymap["#cvm.resetkeys"]<<"\"\n";
-	{
-		std::size_t i=0;
-		for (auto& page : pages) {
-			// Use files instead of aliases since the length limit hurts this program with aliases.
-			std::ofstream selectmenucmd(outputdir.string()+"/cfg/$menu="+page.first.formatted_title+".cfg");
-			selectmenucmd<<"_cvm.mmenu_fix"<<i<<"\nalias _cvm.openmenu \"exec openpage("<<page.first.formatted_title<<")\"";
-			multimenu_fix<<"alias _cvm.mmenu_fix"<<i<<"\"alias +cvm.openmenu _cvm.cvmstate0\"\n";
-			i++;
-		}
-	}
-	exec << "alias _cvm.openmenu ;\n";
+)";
+	for (std::size_t i=0u; i < pages.size(); i++) multimenu_fix<<"alias _cvm.mmenu_fix"<<i<<"\"alias +cvm.openmenu _cvm.cvmstate0\"\n";
 	// Conversion to UCS-2.
 	std::locale utf16(std::locale::classic(),new std::codecvt_utf16<wchar_t, 0xffff, std::little_endian>);
 	std::cout<<"Done. Creating caption file.\n";
@@ -92,7 +79,7 @@ alias _cvm.resetkeys ")"<<keymap["#cvm.resetkeys"]<<"\"\n";
 		// If a page exists with the same name, append a number to the end.
 		std::ofstream cfgfile(cfgpath);
 		// Write to cfg
-		cfgfile<<"cc_emit _#cvm.clear_screen\ncc_emit _#cvm."+page->first.formatted_title+'\n';
+		cfgfile<<"_cvm.menusettings\ncc_emit _#cvm.clear_screen\ncc_emit _#cvm."+page->first.formatted_title+'\n';
 		{
 			unsigned long temptogglenumber=togglenumber;
 			for (auto kbind=page->first.binds.begin(); kbind!=page->first.binds.end(); kbind++) {
@@ -117,7 +104,6 @@ alias _cvm.resetkeys ")"<<keymap["#cvm.resetkeys"]<<"\"\n";
 				if (keymap["#cvm.italicizedbydefault"].find("true")!=std::string::npos) captionfile<<"<I>";
 				captionfile<<convert.from_bytes(keymap["#cvm.defaultcolor"]);
 			}
-		for (auto kbind=page->first.binds.begin(); kbind!=page->first.binds.end(); kbind++) {
 			if (kbind->istogglebind==true) {
 				exec<<"alias _cvm.toggle_"<<std::to_string(togglenumber)<<" _cvm.toggle_"<<std::to_string(togglenumber)<<"_0\n";
 				exec<<"alias _#cvm.toggle_"<<std::to_string(togglenumber)<<" _#cvm.toggle_"<<std::to_string(togglenumber)<<"_0\n";
