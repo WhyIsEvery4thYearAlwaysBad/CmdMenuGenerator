97c4acd9ea323eb74f29ab70c632b1fda02c1895
WhyIsEvery4thYearAlwaysBad <amicdict163@gmail.com>
2020-11-25 12:53:40 -0600

    Removed unneeded macro.

--- src/tokens.cpp
+++ src/tokens.cpp
@@ -3,7 +3,6 @@
 #include <deque>
 #include <iostream>
 #include <map>
-#define SPACE (' ' || '\r' || '\t' || '\v' || '\n')
 #define GetPrevChar(str,i,c) str.rfind(c,i-1)
 #define GetNextChar(str,i,c) str.find(c,i+1)
 extern std::map<std::string,std::string> keymap;
