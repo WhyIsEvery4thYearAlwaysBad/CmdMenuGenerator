a3c7b67b9009eb331d818eb9c3d56273d934e036
WhyIsEvery4thYearAlwaysBad <amicdict163@gmail.com>
2021-01-24 14:43:23 -0600

    God dannit…

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -391,7 +391,7 @@ void MenuCreate(unsigned short& bindcount) {
 				tpage=static_cast<Parser::PageToken&>(**t);
 				std::size_t i=0llu;
 				// Form duplicates if formatted name is already taken.
-				if (pagestack.size()>0) for (auto p=pagestack.end(); p!=pagestack.begin(); p--)
+				if (pagestack.size()>1) for (auto p=pagestack.end(); p!=pagestack.begin(); p--)
 				{
 					if (Format(p->first.title)==Format(tpage.Name)) i++;
 				}
