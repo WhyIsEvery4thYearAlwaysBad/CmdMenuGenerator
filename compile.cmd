feae09f7674cf8ea276cbb184ccb38336b310c60
WhyIsEvery4thYearAlwaysBad <amicdict163@gmail.com>
2021-02-08 21:53:31 -0600

    Proper syntax on variables now.

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,11 @@
 @echo off
-cls
-pushd src
-g++ binds.cpp page.cpp launchoptions.cpp compiler.cpp tokens.cpp main.cpp -o ../cvm_generate.exe -std=c++2a -Wall
+pushd C:\Users\Owner\Documents\Projects\VoicemenuGenerator\src
+g++ -m32 binds.cpp page.cpp launchoptions.cpp compiler.cpp tokens.cpp main.cpp -o ../cvm_generate_x32.exe -std=c++2a -Wall
+if NOT %%ERRORLEVEL%% EQU 0 (
+pause
+)
+g++ -m64 binds.cpp page.cpp launchoptions.cpp compiler.cpp tokens.cpp main.cpp -o ../cvm_generate_x64.exe -std=c++2a -Wall
+if NOT %%ERRORLEVEL%% EQU 0 (
+pause
+)
 popd
