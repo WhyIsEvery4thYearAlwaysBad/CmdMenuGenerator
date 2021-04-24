824899512fe19191da848b6e78a39bf3f9082fd2
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-04-23 19:52:20 -0500

    Improved compilation BASH script.

--- compile.sh
+++ compile.sh
@@ -1,12 +1,10 @@
 pushd C:/Users/Owner/Documents/Projects/CmdMenuGenerator/src
-g++ -g -m32 * -o ../cmg-x32.exe -std=c++2a -Wall
+g++ -m32 * -o ../cmg-x32 -std=c++2a
 if [ ! $? -eq 0 ]
 then
 	read -n1 -r -p "Error(s) have occurred here! Press a key to continue."
 fi
-g++ -g -m64 * -o ../cmg-x64.exe -std=c++2a -Wall
-if [ ! $? -eq 0 ]
-then
-	read -n1 -r -p "Error(s) have occurred here! Press a key to continue."
-fi
+g++ -m64 * -o ../cmg-x64 -std=c++2a
+
 popd
+
