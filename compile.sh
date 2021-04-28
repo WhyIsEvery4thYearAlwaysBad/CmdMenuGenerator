20f22a2c22fd6208b28170faa18aeeb0c1b6a643
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-04-27 21:58:00 -0500

    Renamed executables again. Should be the last time.

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 pushd C:/Users/Owner/Documents/Projects/CmdMenuGenerator/src
-g++ -m32 * -o ../cmg-x32 -std=c++2a
+g++ -g -m32 * -o ../cmg-x32 -std=c++2a
 if [ ! $? -eq 0 ]
 then
 	read -n1 -r -p "Error(s) have occurred here! Press a key to continue."
 fi
-g++ -m64 * -o ../cmg-x64 -std=c++2a
+g++ -g -m64 * -o ../cmg-x64 -std=c++2a
 
 popd
 
