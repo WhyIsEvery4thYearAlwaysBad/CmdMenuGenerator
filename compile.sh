008d8ed00a6f32c032b46d90b55247059610c6f4
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-05-02 22:57:42 -0500

    Remove -g launch option.

--- compile.sh
+++ compile.sh
@@ -1,10 +1,9 @@
 pushd C:/Users/Owner/Documents/Projects/CmdMenuGenerator/src
-g++ -g -m32 * -o ../cmg-x32 -std=c++2a
+g++ -m32 * -o ../cmg-x32 -std=c++2a
 if [ ! $? -eq 0 ]
 then
 	read -n1 -r -p "Error(s) have occurred here! Press a key to continue."
 fi
-g++ -g -m64 * -o ../cmg-x64 -std=c++2a
-
+g++ -m32 * -o ../cmg-x64 -std=c++2a
 popd
 
