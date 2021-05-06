0c4db370f53f3894d70e8254a1ed204036d7566d
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-05-06 16:19:44 -0500

    Add icu4c stuff.

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 pushd C:/Users/Owner/Documents/Projects/CmdMenuGenerator/src
-g++ -m32 * -o ../cmg-x32 -std=c++2a
+g++ -m32 -I"../utils/icu4c/include" * -o ../cmg-x32 -std=c++2a
 if [ ! $? -eq 0 ]
 then
 	read -n1 -r -p "Error(s) have occurred here! Press a key to continue."
 fi
-g++ -m32 * -o ../cmg-x64 -std=c++2a
+g++ -m64 -I"../utils/icu4c/include" * -o ../cmg-x64 -std=c++2a
 popd
 
