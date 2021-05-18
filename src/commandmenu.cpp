1d24b5891397ab07c5144f92d3765ead869a5173
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-05-18 16:28:11 -0500

    Added fallback else for invalid values.

--- src/commandmenu.cpp
+++ src/commandmenu.cpp
@@ -9,16 +9,17 @@ CommandMenu::CommandMenu() {}
 
 CommandMenu::CommandMenu(const std::string& p_sName)
 : sRawName(formatRaw(p_sName)), sName(p_sName) {
-	if (KVMap["display"]=="caption") Display=CMenuDisplayType::CAPTIONS;
-	else if (KVMap["display"]=="console") Display=CMenuDisplayType::CONSOLE;
-	else if (KVMap["display"]=="none") Display=CMenuDisplayType::NONE;
+	if (KVMap["display"]=="caption") Display = CMenuDisplayType::CAPTIONS;
+	else if (KVMap["display"]=="console") Display = CMenuDisplayType::CONSOLE;
+	else if (KVMap["display"]=="none") Display = CMenuDisplayType::NONE;
+	else Display=CMenuDisplayType::CAPTIONS;
 }
 	
 CommandMenu::CommandMenu(const std::string& p_sRawName, const std::string& p_sName)
 : sRawName(p_sRawName), sName(p_sName) {
-	if (KVMap["display"]=="caption") Display=CMenuDisplayType::CAPTIONS;
-	else if (KVMap["display"]=="console") Display=CMenuDisplayType::CONSOLE;
-	else if (KVMap["display"]=="none") Display=CMenuDisplayType::NONE;
+	if (KVMap["display"]=="caption") Display = CMenuDisplayType::CAPTIONS;
+	else if (KVMap["display"]=="console") Display = CMenuDisplayType::CONSOLE;
+	else if (KVMap["display"]=="none") Display = CMenuDisplayType::NONE;
 	else Display=CMenuDisplayType::CAPTIONS;
 }
 
