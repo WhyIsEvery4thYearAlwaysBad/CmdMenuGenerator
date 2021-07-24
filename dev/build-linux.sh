9cb0c1ce1bafe8eb5d5e6f5f52b3155586360ef9
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-07-24 02:59:18 -0400

    Statically link linux binaries.

--- dev/build-linux.sh
+++ dev/build-linux.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-# Compiles program for linux.
-COMPILER_X32="g++-10 -m32 * -o ../cmg-x32 -std=c++2a"
-COMPILER_X64="g++-10 -m64 * -o ../cmg-x64 -std=c++2a"
-# Script
-## Exec in script dir.
+# Compile the program for linux.
+COMPILER_X32="g++-10 -m32 * -o ../cmg-x32 -std=c++2a -static"
+COMPILER_X64="g++-10 -m64 * -o ../cmg-x64 -std=c++2a -static"
+# Set work directory to script's parent directory.
 BINDIR=$(dirname "$(readlink -fn "$0")")
 cd "${BINDIR}" || exit 2
-## Compile stuff
+# The actual script.
 cd ../src > /dev/null || exit
 if ${COMPILER_X32} "${@}"
 then
