964e0c981ce7527b1c046c58430d63217509fccf
WhyIsEvery4thYearAlwaysBad <AkalaAmiBro@protonmail.com>
2021-07-24 02:43:38 -0400

    Separate Windows and Linux build processes into different scripts.
    Also only compile the program in 64-bit if no errors occur in 32-bit compilation.

---
+++ dev/build-windows.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Compile program for windows.
+# NOTE: Since <thread> is used, MinGW users are going to need to use POSIX MinGW.
+COMPILER_X32="i686-w64-mingw32-g++-posix * -o ../cmg-x32 -std=c++2a -static"
+COMPILER_X64="x86_64-w64-mingw32-g++-posix * -o ../cmg-x64 -std=c++2a -static"
+# Set work directory to script's parent directory.
+BINDIR=$(dirname "$(readlink -fn "$0")")
+cd "${BINDIR}" || exit 2
+# The actual script.
+cd ../src > /dev/null || exit
+if ${COMPILER_X32} "${@}"
+then
+	${COMPILER_X64} "${@}"
+else
+	printf '\033[31mFatal: Errors have occurred in MingW complation!\033[0m'
+fi
+# Done!
